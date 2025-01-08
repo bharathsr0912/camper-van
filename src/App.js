cc2b09ad41ab56fab6ce707ca84fe119c1cc360a
Bharath SR <bharath.sr@ananest.com>
2025-01-08 19:14:29 -0400

    Add vandetails.js page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans"
+import VanDetail from "./pages/VanDetails"
+
 import "./server"
 
 function App() {
@@ -18,6 +20,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   );
