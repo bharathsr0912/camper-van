ba93b89152cda96dffec3aa5b679c89f0782e3d6
Bharath SR <bharath.sr@ananest.com>
2025-01-10 12:21:44 -0400

    Add host and footer related changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans/Vans"
 import VanDetail from "./pages/Vans/VanDetails"
+import Dashboard from "./pages/Host/Dashboard"
+import Income from "./pages/Host/Income"
+import Reviews from "./pages/Host/Reviews"
 import Layout from "./components/Layout"
+import HostLayout from "./components/HostLayout"
 
 import "./server"
 
@@ -13,9 +17,16 @@ function App() {
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route path="about" element={<About />} />
+        <Route path="vans" element={<Vans />} />
+        <Route path="vans/:id" element={<VanDetail />} />
+
+        <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+        </Route>
+
       </Route>
     </Routes>
   </BrowserRouter>
